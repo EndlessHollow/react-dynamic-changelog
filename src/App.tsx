f3d216f6de6ae2fe1c35211dc49d4f0dc0de207f
Marek Klier <marek.klier.personal@gmail.com>
2021-05-24 21:49:05 +0200

    feat: add project setup (#1)
    * feat: add project setup
    
    * feat: add styled-components and meyer's css reset
    
    Co-authored-by: EndlessHollow <marekklier.personal@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GlobalStyle from './utils/global-style';
+
+const App = () => {
+
+  return (
+    <>
+    <GlobalStyle />
+    <h1>App</h1>
+    </>
+  )
+}
+
+export default App
